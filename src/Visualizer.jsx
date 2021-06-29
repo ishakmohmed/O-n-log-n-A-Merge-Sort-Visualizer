c661393b34109d4f9219e31e5736f0024e9d7921
Mohmed Ishak <ishakmohmed1911@gmail.com>
2021-06-30 01:47:40 +0800

    Visualizer works, but ugly.

---
+++ src/Visualizer.jsx
@@ -0,0 +1,73 @@
+import React, { useEffect, useState } from "react";
+import { getMergeSortAnimations } from "./sortingAlgorithms.js";
+import "./Visualizer.css";
+
+const ANIMATION_SPEED_MS = 2;
+const NUMBER_OF_ARRAY_BARS = 200;
+const PRIMARY_COLOR = "#FE6D73";
+const SECONDARY_COLOR = "#50FFB1";
+
+function Visualizer() {
+  const [array, setArray] = useState([]);
+
+  useEffect(() => {
+    resetArray();
+  }, []);
+
+  function resetArray() {
+    const array = [];
+    for (let i = 0; i < NUMBER_OF_ARRAY_BARS; i++) {
+      array.push(randomIntFromInterval(5, 730));
+    }
+    setArray(array);
+  }
+
+  function mergeSort() {
+    const animations = getMergeSortAnimations(array);
+    for (let i = 0; i < animations.length; i++) {
+      const arrayBars = document.getElementsByClassName("array-bar");
+      const isColorChange = i % 3 !== 2;
+      if (isColorChange) {
+        const [barOneIdx, barTwoIdx] = animations[i];
+        const barOneStyle = arrayBars[barOneIdx].style;
+        const barTwoStyle = arrayBars[barTwoIdx].style;
+        const color = i % 3 === 0 ? SECONDARY_COLOR : PRIMARY_COLOR;
+        setTimeout(() => {
+          barOneStyle.backgroundColor = color;
+          barTwoStyle.backgroundColor = color;
+        }, i * ANIMATION_SPEED_MS);
+      } else {
+        setTimeout(() => {
+          const [barOneIdx, newHeight] = animations[i];
+          const barOneStyle = arrayBars[barOneIdx].style;
+          barOneStyle.height = `${newHeight}px`;
+        }, i * ANIMATION_SPEED_MS);
+      }
+    }
+  }
+
+  function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+  }
+
+  return (
+    <div className="array-container">
+      {array.map((value, idx) => (
+        <div
+          className="array-bar"
+          key={idx}
+          style={{
+            backgroundColor: PRIMARY_COLOR,
+            height: `${value}px`,
+          }}
+        ></div>
+      ))}
+      <button onClick={() => resetArray()}>
+        RESET BARS
+      </button>
+      <button onClick={() => mergeSort()}>MERGE SORT</button>
+    </div>
+  );
+}
+
+export default Visualizer;
